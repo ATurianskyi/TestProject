ce9464481df0146e13a6901dd85c77b44a7df864
ATurianskyi <andrii.turianskyi1@gmail.com>
2023-08-10 16:10:01 +0300

    Added chromeOptions

--- src/test/java/org/example/TestBase.java
+++ src/test/java/org/example/TestBase.java
@@ -7,20 +7,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.concurrent.TimeUnit;
-
 public class TestBase {
     public static WebDriver driver;
 
     @Before
     public void setup() {
         ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(chromeOptions);
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @After
