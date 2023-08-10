459a533ddff9954ed48e00f775588dc66c2b642f
ATurianskyi <andrii.turianskyi1@gmail.com>
2023-08-10 14:31:24 +0300

    Removed Util class

--- src/main/java/org/example/Util.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.nio.file.Paths;
-
-public class Util {
-    public static String getPathToProjectFolder() {
-        return Paths.get("").toAbsolutePath().toString();
-    }
-}
